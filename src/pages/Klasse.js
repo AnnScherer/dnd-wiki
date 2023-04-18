982a4ef34134a229c4351aa47c13a8e0f41010ce
AnnScherer <ann.hardt@gmx.de>
2023-04-18 19:47:55 +0200

    fixed tabelle

--- src/pages/Klasse.js
+++ src/pages/Klasse.js
@@ -14,49 +14,45 @@ export default function Klasse() {
   console.log(klassen);
 
   return (
-    <div className="völker">
-      <div className="select">
-        <div className="select-volk">
-          <label>Klasse auswählen:</label>
-          <select onChange={onChangeKlasse}>
-            <option>Klasse auswählen</option>
-            <option>BARBAR</option>
-            <option>BARDE</option>
-            <option>DRUIDE</option>
-            <option>HEXENMEISTER</option>
-            <option>KÄMPFER</option>
-            <option>KLERIKER</option>
-            <option>MAGIER</option>
-            <option>MÖNCH</option>
-            <option>PALADIN</option>
-            <option>SCHURKE</option>
-            <option>WALDLÄUFER</option>
-            <option>ZAUBERER</option>
-          </select>
+    <div className="container">
+      <div className="völker">
+        <div className="select">
+          <div className="select-volk">
+            <label>Klasse auswählen:</label>
+            <select onChange={onChangeKlasse}>
+              <option>Klasse auswählen</option>
+              <option>BARBAR</option>
+              <option>BARDE</option>
+              <option>DRUIDE</option>
+              <option>HEXENMEISTER</option>
+              <option>KÄMPFER</option>
+              <option>KLERIKER</option>
+              <option>MAGIER</option>
+              <option>MÖNCH</option>
+              <option>PALADIN</option>
+              <option>SCHURKE</option>
+              <option>WALDLÄUFER</option>
+              <option>ZAUBERER</option>
+            </select>
+          </div>
         </div>
+        {filterKlasse.length > 0 && (
+          <>
+            {filterKlasse.map((klasse, i) => {
+              return (
+                <div className="volk" key={i}>
+                  <h3 key={i}>{klasse.name}</h3>
+                  {klasse.text.map((info, i) => {
+                    return (
+                      <p dangerouslySetInnerHTML={{ __html: info }} key={i}></p>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
-      {filterKlasse.length > 0 && (
-        <>
-          {filterKlasse.map((klasse, i) => {
-            return (
-              <div className="volk" key={i}>
-                <h3 key={i}>{klasse.name}</h3>
-                <p
-                  className="tabelle"
-                  id={klasse.name.toLowerCase()}
-                  dangerouslySetInnerHTML={{ __html: klasse.tabelle }}
-                  key={klasse.name}
-                ></p>
-                {klasse.text.map((info, i) => {
-                  return (
-                    <p dangerouslySetInnerHTML={{ __html: info }} key={i}></p>
-                  );
-                })}
-              </div>
-            );
-          })}
-        </>
-      )}
     </div>
   );
 }
